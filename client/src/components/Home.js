f3ce7db41bb63fb5b2883f7c56095cf11c4db0f3
interdigitize <interdigitize@users.noreply.github.com>
2018-01-25 21:47:01 -0800

    Starts to configure file size and file type error handling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,10 +9,14 @@ class Home extends Component {
       file: undefined,
       ascii: '',
       submitDisabled: true,
-      selectDisabled: false
+      selectDisabled: false,
+      error: false
     };
     this.upload = this.upload.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.validate = this.validate.bind(this);
+    this.reset = this.reset.bind(this);
+    this.errors = [];
   }
 
   upload(e) {
@@ -46,20 +50,71 @@ class Home extends Component {
 
   onChange(e) {
     e.preventDefault();
-    console.log('file info', e.target.files[0]);
-    let formData = new FormData();
-    formData.append('file', e.target.files[0]);
+    // console.log('file info', e.target.files[0]);
     this.setState({
-      file: formData,
-      submitDisabled: false
+      error: false,
+      ascii: ''
+    });
+    this.validate(e.target.files[0]);
+  }
+
+  reset() {
+    this.setState({
+      file: undefined,
+      ascii: '',
+      submitDisabled: true,
+      selectDisabled: false,
+      error: false
     });
   }
 
+  validate(file) {
+    if (typeof file.type !== 'undefined') {
+      if (
+        file.type === 'image/jpeg' ||
+        file.type === 'image/jpg' ||
+        file.type === 'image/png' ||
+        file.type === 'image/gif'
+      ) {
+        if (file.size < 5000000) {
+          let formData = new FormData();
+          formData.append('file', file);
+          this.setState({
+            file: formData,
+            submitDisabled: false
+          });
+        } else {
+          this.setState(
+            {
+              error: true,
+              submitDisabled: true
+            },
+            () => this.errors.push('size')
+          );
+        }
+      } else {
+        // console.log('error in else', this.state.error);
+        this.setState(
+          {
+            error: true,
+            submitDisabled: true
+          },
+          () => this.errors.push('type')
+        );
+      }
+    } else {
+      this.reset();
+    }
+  }
+
   render() {
+    var errors = {
+      size: 'Please select a file smaller than 5MB.',
+      type: 'Please select a jpg or png.'
+    };
     return (
       <div className="content">
         <h2>Upload an image to convert it.</h2>
-
         <form id="upload">
           <input
             type="file"
@@ -72,6 +127,17 @@ class Home extends Component {
             disabled={this.state.submitDisabled}>
             ASCII It
           </button>
+          {/* {console.log('Error', this.state.error, this.errors)} */}
+          {this.state.error ? (
+            <div>
+              {this.errors.map(error => {
+                // console.log(errors, errors[error]);
+                return <p>{errors[error]}</p>;
+              })}
+            </div>
+          ) : (
+            <span />
+          )}
         </form>
         {this.state.selectDisabled === true ? (
           <div class="spinner">
